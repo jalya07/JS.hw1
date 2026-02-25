917bba2f4c2a4e8587eb07891bcbf9c724c2eabd
jalya07 <jalavh@code.edu.az>
2026-02-26 03:26:49 +0400

    add

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// var n = 5;
+// var m = 15;
+// var count = 0;
+
+// while (n <= m) {
+//     if (n % 2 != 0) {
+//         count++;
+//     }
+//     n++;
+// }
+
+// console.log(count);
+var n=5;
+var m=15;
+var sum=0;
+
+while(n<m){
+    if(n%2 !=0){
+        sum+=n;
+    }
+    n++;
+}
+console.log(sum);
+
+
+
+
