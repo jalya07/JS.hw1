4650e271f736b6633601b20ba6a43cf907152d5e
jalya07 <jalavh@code.edu.az>
2026-02-27 13:01:12 +0400

    change

--- js/script.js
+++ js/script.js
@@ -10,17 +10,31 @@
 // }
 
 // console.log(count);
-var n=5;
-var m=15;
-var sum=0;
+// var n=5;
+// var m=15;
+// var sum=0;
 
-while(n<m){
-    if(n%2 !=0){
-        sum+=n;
-    }
-    n++;
-}
-console.log(sum);
+// while(n<m){
+//     if(n%2 !=0){
+//         sum+=n;
+//     }
+//     n++;
+// }
+// console.log(sum);
+
+// var n=10;
+// for(var i=2;i<n;i++){
+//     if(n%i==0){
+//         console.log("sostavnoe");
+//     }
+//     else
+//     {
+//         console.log("prostoe");
+//         break;
+//     }
+// }
+
+
 
 
 
